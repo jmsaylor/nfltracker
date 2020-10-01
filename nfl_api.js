a104b0b23a3871a76502d00bc664733dfe0593a2
John <johnmsaylor@gmail.com>
2020-10-01 14:20:38 -0400

    update

--- nfl_api.js
+++ nfl_api.js
@@ -1,27 +1,24 @@
-
-require('dotenv/config')
-// For interacting with Sport Radar API 
-//--------------------------------------------------
+require("dotenv/config");
+// For interacting with Sport Radar API
+//-------------------------------------------------
+//
 
 //const url_path = 'nfl/official/trial/v5/en/seasons/2019/standings.json' //for standings
 
-
 async function nfl_teamprofile(team_id) {
-    const fetch = require('node-fetch')
-    const key = process.env.API_KEY
-    const url_path = `nfl/official/trial/v5/en/teams/${team_id}/profile.json`
-    const api_url = `https://api.sportradar.us/${url_path}?api_key=${key}`
-    try {
-        const response = await fetch(api_url)
-        if (!response.ok) {
-            throw new Error("network response no good")
-        } 
-        const myJSON = await response.json()
-
-        return myJSON
-    } catch (error) {
-        console.log('try failed', error.message)
+  const fetch = require("node-fetch");
+  const key = process.env.API_KEY;
+  const url_path = `nfl/official/trial/v5/en/teams/${team_id}/profile.json`;
+  const api_url = `https://api.sportradar.us/${url_path}?api_key=${key}`;
+  try {
+    const response = await fetch(api_url);
+    if (!response.ok) {
+      throw new Error("network response no good");
     }
+    const myJSON = await response.json();
+
+    return myJSON;
+  } catch (error) {
+    console.log("try failed", error.message);
+  }
 }
-
-
